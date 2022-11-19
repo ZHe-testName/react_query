306433118e94ae5162b96eb91e4bb3627268adeb
Zheka Horunzhii <horunzhy.wgen@gmail.com>
2022-11-19 14:27:45 +0200

    Prepare all files and tools

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools';
+
+const client = new QueryClient();
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <QueryClientProvider client={client}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+    <ReactQueryDevtools initialIsOpen={false}/>
+  </QueryClientProvider>
+);
+
