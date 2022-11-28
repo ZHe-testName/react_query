4472679fbbdef2782c6d97f5da80222b7b0a61d2
Zheka Horunzhii <horunzhy.wgen@gmail.com>
2022-11-28 23:57:19 +0200

    Finished

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -1,27 +1,41 @@
 import axios from "axios";
 import { useState } from "react";
-import { useQuery } from "react-query";
+import { useInfiniteQuery } from "react-query";
+import React from "react";
 
-export default function Pagination() {
-  const [page, setPage] = useState(0);
-
-  const postsQuery = useQuery(
-    ['posts', { page }],
-    () => {
-      return axios
+function fetchPosts({pageParam = 0}) {//receive object as useQuery receive as poop
+  console.log('gggggggg', pageParam);
+    return axios
         .get(`https://mockend.com/ZHe-testName/mockend/posts`, {
           params: {
-            limit: 10,
-            offset: page * 10,
+            limit: 100,
+            offset: pageParam * 10,
           },
         })
         .then(res => res.data);
-    },
-    {
-      refetchOnWindowFocus: false,
-    }
-  );
+  };
 
+export default function Pagination() {
+  const [page, setPage] = useState(0);
+  // const queryClient = useQueryClient();
+
+  // also we can use InfiniteQuery for infinite pagination
+  const postsQuery = useInfiniteQuery('posts', fetchPosts, {
+    getNextPageParam: (lastPage, allPages) => {
+      console.log('gggggggggggggg', allPages);
+      return lastPage.nextCursor
+    }
+  });
+
+  // const postsQuery = useQuery(
+  //   ['posts', { page }],
+  //   fetchPosts,
+  //   {
+  //     refetchOnWindowFocus: false,
+  //     keepPreviousData: true,
+  //   }
+  // );
+console.log(postsQuery);
   return (
     <>
       {
@@ -37,7 +51,25 @@ export default function Pagination() {
 
               <ul>
                 {
-                  postsQuery.isFetched && postsQuery.data.map(post => 
+                  postsQuery.isFetched && postsQuery.data?.pages?.map((page, idx) => {
+                    return <React.Fragment key={idx}>
+                      {page.map(post => 
+                        <li 
+                          key={post.id}
+                        >
+                          <span>{post.id}</span> - <span>{post.title}</span>
+                          <p>{post.createdAt}</p>
+                          <hr/>
+                        </li>
+                      )}
+                    </React.Fragment>
+                  })
+                }
+              </ul>
+
+              {/* <ul>
+                {
+                  postsQuery.isFetched && postsQuery.data?.map(post => 
                   <li
                     key={post.id}
                   >
@@ -46,15 +78,26 @@ export default function Pagination() {
                     <hr/>
                   </li>)
                 } 
-              </ul>
+              </ul> */}
             </div>
       }
 
-      <div>
-        <button onClick={() => setPage(old => old - 1)}>Prev</button>{'  '}
-        <button onClick={() => setPage(old => old + 1)}>Next</button>
+      {/* <div>
+        <button 
+          onClick={() => setPage(old => old - 1)}
+          disabled={page <= 0}
+        >Prev</button>{'  '}
+        <button
+           onClick={() => setPage(old => old + 1)}
+           disabled={page >= 9}
+        >Next</button>
         <br/>
         <span>Current Page: {page + 1} {postsQuery.isFetching ? '...' : ''}</span>
+      </div> */}
+      <div>
+        <button
+
+        ></button>
       </div>
     </>
   );
